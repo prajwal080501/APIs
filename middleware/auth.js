f6b87a0c3a2794345155bf18dd5992601355a918
Prajwal Ladkat <praju.ladkat@gmail.com>
2022-12-06 17:14:33 +0530

    changed import index.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const ErrorResponse = require("../utils/errorResponse");
+const ErrorResponse = require("../utils/errorresponse");
 
 exports.protect = async (req, res, next) => {
     let token;
